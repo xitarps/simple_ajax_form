72bc447fee7b1510292251cb20ddc7e124f0b69e
XitaRps <xitarps@gmail.com>
2023-05-06 18:13:40 -0300

    first commit

---
+++ lib/simple_ajax_form.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "simple_ajax_form/version"
+
+module SimpleAjaxForm
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/simple_ajax_form_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe SimpleAjaxForm do
+  it "has a version number" do
+    expect(SimpleAjaxForm::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
